3d5ac789b79cfe9382087b1ac286548c34c8ffa7
Chrisderennes <cg35@mail.fr>
2026-05-02 14:06:45 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,30 @@
-const CACHE_NAME = 'arena-v3';
+const CACHE_NAME = 'arena-v3'; // On passe en v3 ici
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html', './manifest.json', './logo2.gif'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => 
+      cache.addAll(['./', './index.html', './manifest.json', './logo2.gif'])
+    )
+  );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
+
+// Le bloc à ajouter pour nettoyer les vieux dossiers de cache
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
         keys.map((key) => {
           if (key !== CACHE_NAME) {
-            return caches.delete(key); // On supprime l'ancien cache v2
+            return caches.delete(key);
           }
         })
       );
     })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
